eef37db99597448e0cbdb5162fa02e8d97889379
User <user@MacBook-Pro-108309.local>
2024-06-24 00:56:01 +0300

    cors anywhere

--- server.js
+++ server.js
@@ -1,66 +1,12 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const cors = require('cors');
+const corsAnywhere = require('cors-anywhere');
 
-const app = express();
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8080;
 
-const corsOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-// Разрешить CORS для всех маршрутов
-app.use(cors(corsOptions));
-
-// Прокси для API запросов
-app.use('/api', createProxyMiddleware({
-  target: 'https://platform.fintacharts.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '/api', // Оставьте без изменений
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    proxyReq.setHeader('origin', 'https://platform.fintacharts.com');
-  },
-  onProxyRes: (proxyRes, req, res) => {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
-  }
-}));
-
-// Прокси для запросов на авторизацию
-app.use('/identity', createProxyMiddleware({
-  target: 'https://platform.fintacharts.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/identity': '/identity',
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    proxyReq.setHeader('origin', 'https://platform.fintacharts.com');
-  },
-  onProxyRes: (proxyRes, req, res) => {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    proxyRes.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
-  }
-}));
-
-// Прокси для WebSocket запросов
-app.use('/wss', createProxyMiddleware({
-  target: 'wss://platform.fintacharts.com',
-  changeOrigin: true,
-  ws: true,
-  pathRewrite: {
-    '^/wss': '/api/streaming/ws/v1/realtime', // перепишите путь для WebSocket
-  },
-  onProxyReqWs: (proxyReq, req, socket, options, head) => {
-    proxyReq.setHeader('origin', 'wss://platform.fintacharts.com');
-  }
-}));
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Proxy server is running on port ${PORT}`);
+corsAnywhere.createServer({
+originWhitelist: [], // Разрешить все источники
+requireHeader: ['origin', 'x-requested-with'],
+removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, () => {
+console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
